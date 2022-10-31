dc22d359e7245198970db099ea655350e62527be
codingLogan <reachCodingLogan@gmail.com>
2022-10-31 09:15:22 -0600

    Update workflow

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-function printMsg(message?: string): void {
+export function printMsg(message?: string): void {
   console.log(`This is a message from the demo package`);
 
   if (message) {
     console.log("Your message", message);
   }
 }
-
-// commonjs export
-exports.printMsg = printMsg;
