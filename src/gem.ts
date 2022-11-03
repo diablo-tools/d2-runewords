76836e7a19763c23eefdd5fbca7cf83b278c2ec0
Logan Rasmussen <reachCodingLogan@gmail.com>
2022-11-02 21:44:14 -0600

    Add runes (#20)
    * Reset package version
    
    * Add rune structure to codebase
    
    * Add simple ItemEffect type

---
+++ src/gem.ts
@@ -0,0 +1,4 @@
+export type Gem = {
+  name: "Topaz" | "Amethyst" | "Sapphire" | "Ruby" | "Emerald" | "Diamond";
+  quality: "Chipped" | "Flawed" | "" | "Flawless" | "Perfect";
+};
---
+++ src/rune.ts
@@ -0,0 +1,57 @@
+import { Gem } from "./gem";
+import { ItemEffect } from "./itemEffect";
+
+export type RuneName =
+  | "El"
+  | "Eld"
+  | "Tir"
+  | "Nef"
+  | "Eth"
+  | "Ith"
+  | "Tal"
+  | "Ral"
+  | "Ort"
+  | "Thul"
+  | "Amn"
+  | "Sol"
+  | "Shael"
+  | "Dol"
+  | "Hel"
+  | "Io"
+  | "Lum"
+  | "Ko"
+  | "Fal"
+  | "Lem"
+  | "Pul"
+  | "Um"
+  | "Mal"
+  | "Ist"
+  | "Gul"
+  | "Vex"
+  | "Ohm"
+  | "Lo"
+  | "Sur"
+  | "Ber"
+  | "Jah"
+  | "Cham"
+  | "Zod";
+
+export type RuneRecipeItem = {
+  item: RuneName | Gem;
+  quantity: number;
+};
+
+export type RuneRecipe = {
+  rune: RuneName;
+  items: Array<RuneRecipeItem>;
+};
+
+export type Rune = {
+  name: RuneName;
+
+  image: string;
+  level: number;
+
+  armor: ItemEffect;
+  weapon: ItemEffect;
+};
