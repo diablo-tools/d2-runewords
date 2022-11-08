b6703d41381861a50f25ab3240bb30820aac7dc5
codingLogan <reachCodingLogan@gmail.com>
2022-11-07 21:55:21 -0700

    Add a games filter

--- src/functions/filterWords.ts
+++ src/functions/filterWords.ts
@@ -1,10 +1,13 @@
 import { ItemType } from "../types/items";
 import { NumberOfSockets, Runeword } from "../types/runeword";
 
+type Games = "diablo2" | "diablo2Resurrected";
+
 // These filters are arrays to allow multiple selections
 type WordFilters = {
   itemType: ItemType[];
   sockets: NumberOfSockets[];
+  games: Games[];
 };
 
 function arraysIntersect(array1: string[], array2: string[]) {
@@ -28,6 +31,16 @@ export function filterWords(words: Runeword[], filters: WordFilters) {
     let socketsFound =
       filters.sockets.length === 0 || filters.sockets.includes(word.sockets);
 
-    return socketsFound && itemTypeFound;
+    let gamesFound = false;
+    if (
+      filters.games.length === 0 ||
+      (word.diablo2ResurrectedOnly &&
+        filters.games.includes("diablo2Resurrected")) ||
+      (!word.diablo2ResurrectedOnly && filters.games.includes("diablo2"))
+    ) {
+      gamesFound = true;
+    }
+
+    return socketsFound && itemTypeFound && gamesFound;
   });
 }
--- src/tests/filterWords.test.ts
+++ src/tests/filterWords.test.ts
@@ -2,16 +2,20 @@ import assert from "assert";
 import { filterWords } from "../functions/filterWords";
 import words from "./testWords";
 
+import { getAllWords } from "../functions/getAllWords";
+
 // Socket tests
 const sockets2 = filterWords(words, {
   itemType: [],
   sockets: [2],
+  games: [],
 });
 assert(sockets2.length === 2);
 
 const sockets3 = filterWords(words, {
   itemType: [],
   sockets: [3],
+  games: [],
 });
 assert(sockets3.length === 6);
 
@@ -19,12 +23,14 @@ assert(sockets3.length === 6);
 const itemType = filterWords(words, {
   itemType: ["Scepters"],
   sockets: [],
+  games: [],
 });
 assert(itemType.length === 2);
 
 const combinedTypes = filterWords(words, {
   itemType: ["Maces", "Scepters", "Shields"],
   sockets: [],
+  games: [],
 });
 assert(combinedTypes.length === 4);
 
@@ -32,5 +38,30 @@ assert(combinedTypes.length === 4);
 const selectedSocketsAndFilters = filterWords(words, {
   itemType: ["Maces", "Scepters", "Shields"],
   sockets: [3],
+  games: [],
 });
 assert(selectedSocketsAndFilters.length === 3);
+
+// Sockets and types and games
+const selectedSocketsFiltersGames = filterWords(words, {
+  itemType: ["Maces", "Scepters", "Shields"],
+  sockets: [3],
+  games: ["diablo2"],
+});
+assert(selectedSocketsFiltersGames.length === 3);
+
+// Sockets and types and games
+const selectedSocketsFiltersGames2 = filterWords(words, {
+  itemType: ["Maces", "Scepters", "Shields"],
+  sockets: [3],
+  games: ["diablo2Resurrected"],
+});
+assert(selectedSocketsFiltersGames2.length === 0);
+
+assert(
+  filterWords(getAllWords(), {
+    itemType: [],
+    sockets: [],
+    games: ["diablo2Resurrected"],
+  }).length === 7
+);
