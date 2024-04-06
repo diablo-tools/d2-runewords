791a6472681213c147ee810122493b8f9e874df2
Logan Rasmussen <logan.razclan1@gmail.com>
2024-04-06 12:26:17 -0600

    Update for D2R 2.6 runewords

--- src/functions/getAllWords.ts
+++ src/functions/getAllWords.ts
@@ -3,9 +3,9 @@ import d110 from "../data/runewords/1.10";
 import d110ladder from "../data/runewords/1.10-ladder";
 import d111 from "../data/runewords/1.11";
 import d2r24 from "../data/runewords/d2r-2.4";
+import d2r26 from "../data/runewords/d2r-2.6";
 
 export function getAllWords() {
-  // From all runeword files, find all the items types
-  const allWords = [...d107, ...d110, ...d110ladder, ...d111, ...d2r24];
+  const allWords = [...d107, ...d110, ...d110ladder, ...d111, ...d2r24, ...d2r26];
   return allWords;
 }
